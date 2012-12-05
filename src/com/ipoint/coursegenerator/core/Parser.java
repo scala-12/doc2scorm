dd9a9eed78db03a79a54da6a0c1c3583b75b42ac
Victor Burov <burov@ipoint.ru>
2012-12-05 09:58:28 +0000

    git-svn-id: http://192.168.0.127/svn/ilogos/ilogos-course-generator-core/trunk@60 d436a95e-17fb-11e2-809d-01f0c2aa7d15

--- src/com/ipoint/coursegenerator/core/Parser.java
+++ src/com/ipoint/coursegenerator/core/Parser.java
@@ -3,6 +3,8 @@ package com.ipoint.coursegenerator.core;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+//import java.util.logging.Logger;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,18 +18,30 @@ import org.apache.poi.hwpf.model.PicturesTable;
 import org.apache.poi.hwpf.usermodel.Paragraph;
 import org.apache.poi.hwpf.usermodel.Range;
 import org.apache.poi.poifs.filesystem.OfficeXmlFileException;
+import org.apache.poi.xwpf.usermodel.BodyElementType;
+import org.apache.poi.xwpf.usermodel.IBodyElement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFPicture;
+import org.apache.poi.xwpf.usermodel.XWPFPictureData;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.impl.values.XmlAnyTypeImpl;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTObject;
 import org.w3c.dom.Document;
 
 import com.ipoint.coursegenerator.core.elementparser.ParagraphParser;
 import com.ipoint.coursegenerator.core.utils.FileUtils;
 
 public class Parser {
-    
+
     public final static String FILETYPE_DOCX = ".docx";
 
     public final static String FILETYPE_DOC = ".doc";
-    
+
     private Document createNewHTMLDocument() {
 	try {
 	    Document html = DocumentBuilderFactory.newInstance()
@@ -51,7 +65,7 @@ public class Parser {
 	directory.mkdirs();
 	Object doc = null;
 	if (fileType.toLowerCase().equals(FILETYPE_DOC)) {
-	    doc = new HWPFDocument(stream);	    
+	    doc = new HWPFDocument(stream);
 	} else if (fileType.toLowerCase().equals(FILETYPE_DOCX)) {
 	    doc = new XWPFDocument(stream);
 	}
@@ -66,7 +80,49 @@ public class Parser {
 
     private void parseXWPF(XWPFDocument doc, String headerLevel,
 	    String template, String courseName, String path) {
+	int headerLevelNumber = Integer.parseInt(headerLevel);
+	String level = "";
+	XWPFDocument document = doc;
+	Document html = null;
+	int htmlFileCounter = 0;
+	int paragraphStyle = 0;
+	final List<IBodyElement> bodyElements = document.getBodyElements();
 
+	for (int i = 0; i < bodyElements.size(); i++) {
+	    if (bodyElements.get(i).getElementType()
+		    .equals(BodyElementType.PARAGRAPH)) {
+		XWPFParagraph paragraph = (XWPFParagraph) bodyElements.get(i);
+		if (paragraph.getStyle() != null) {
+		    try {
+			paragraphStyle = Integer.valueOf(paragraph.getStyleID());
+			System.out.println(paragraph.getStyle());
+		    } catch (NumberFormatException e) {		paragraphStyle = 0;
+		    }
+		    if (paragraphStyle > 0
+			    && paragraphStyle <= headerLevelNumber) {
+			if (html != null) {
+			    FileUtils.saveHTMLDocument(html, path
+				    + File.separator + FileUtils.HTML_PREFIX
+				    + Integer.toString(htmlFileCounter)
+				    + ".htm");
+			}
+			html = createNewHTMLDocument();
+		    } else if (html == null) {
+			html = createNewHTMLDocument();
+
+		    }
+		    ParagraphParser.parse(paragraph, html, document, path);
+		}
+		
+	    } else if (bodyElements.get(i).getElementType()
+		    .equals(BodyElementType.TABLE)) {
+
+	    }
+
+	}
+	FileUtils.saveHTMLDocument(html, path + File.separator
+		+ FileUtils.HTML_PREFIX + Integer.toString(htmlFileCounter)
+		+ ".htm");
     }
 
     private void parseHWPF(HWPFDocument doc, String headerLevel,
@@ -95,5 +151,6 @@ public class Parser {
 	FileUtils.saveHTMLDocument(html, path + File.separator
 		+ FileUtils.HTML_PREFIX + Integer.toString(htmlFileCounter)
 		+ ".htm");
+
     }
 }
