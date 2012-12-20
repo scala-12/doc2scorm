ddeeddffb9ae2ac80c002d2f783080a4dd85fc63
Victor Burov <burov@ipoint.ru>
2012-12-20 03:19:30 +0000

    #2
    git-svn-id: http://192.168.0.127/svn/ilogos/ilogos-course-generator-core/trunk@93 d436a95e-17fb-11e2-809d-01f0c2aa7d15

--- src/com/ipoint/coursegenerator/core/elementparser/graphics/RasterGraphicsParser.java
+++ src/com/ipoint/coursegenerator/core/elementparser/graphics/RasterGraphicsParser.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import org.apache.poi.hwpf.usermodel.Picture;
 import org.apache.poi.xwpf.usermodel.Document;
-import org.apache.poi.xwpf.usermodel.XWPFPicture;
+import org.apache.poi.xwpf.usermodel.XWPFPictureData;
 import org.w3c.dom.Element;
 
 import com.ipoint.coursegenerator.core.utils.FileWork;
@@ -15,16 +15,17 @@ public class RasterGraphicsParser extends AbstractGraphicsParser {
 	if (pic.getMimeType().equals(IMAGE_PNG)) {
 	    String url = "img" + File.separator + FileWork.IMAGE_PREFIX
 		    + pic.hashCode() + ".png";
-	    FileWork.savePNGImage(pic.getRawContent(), path
-		    + File.separator + url);
+	    FileWork.savePNGImage(pic.getRawContent(), path + File.separator
+		    + url);
 	    imgElement
 		    .setAttribute("src", url.replace(File.separatorChar, '/'));
 	}
     }
 
-    public static void parse(XWPFPicture pic, String path, Element imgElement) {
+    public static void parse(XWPFPictureData pic, String path,
+	    Element imgElement) {
 	String url = null;
-	switch (pic.getPictureData().getPictureType()) {
+	switch (pic.getPictureType()) {
 	case Document.PICTURE_TYPE_PNG:
 	    url = "img" + File.separator + FileWork.IMAGE_PREFIX
 		    + pic.hashCode() + ".png";
@@ -43,8 +44,7 @@ public class RasterGraphicsParser extends AbstractGraphicsParser {
 	    break;
 	}
 	if (url != null) {
-	    FileWork.savePNGImage(pic.getPictureData()
-		    .getData(), path + File.separator + url);
+	    FileWork.savePNGImage(pic.getData(), path + File.separator + url);
 	    imgElement
 		    .setAttribute("src", url.replace(File.separatorChar, '/'));
 	}
