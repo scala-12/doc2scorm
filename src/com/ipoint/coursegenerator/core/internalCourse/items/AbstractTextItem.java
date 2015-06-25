78401830ae288c5bf751b45415344774570273d7
kalashnikov <kalashnikov@ipoint.ru>
2015-06-26 00:50:33 +0800

    + previous commit

---
+++ src/com/ipoint/coursegenerator/core/internalCourse/items/AbstractTextItem.java
@@ -0,0 +1,17 @@
+package com.ipoint.coursegenerator.core.internalCourse.items;
+
+import com.ipoint.coursegenerator.core.internalCourse.blocks.HyperlinkBlock;
+import com.ipoint.coursegenerator.core.internalCourse.blocks.TextBlock;
+
+/***
+ * Item for {@link TextBlock}
+ * data
+ * 
+ * @see TextBlock
+ * @see HyperlinkBlock
+ * @author Kalashnikov Vladislav
+ *
+ */
+public abstract class AbstractTextItem extends AbstractItem {
+
+}
