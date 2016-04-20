d2f438472c20ac24c68c12571e01b024e73689fa
vlad <kalashnikov@ipoint.ru>
2016-04-20 11:36:45 +0800

    Fixes T931: Tests was updated

--- app/assets/app/services/user.spec.ts
+++ app/assets/app/services/user.spec.ts
@@ -3,12 +3,12 @@ import { User } from './user';
 describe('User', () => {
 
     it('user name test', () => {
-        let user: User = { id: 1, name: "user1", email: "user1@email.com", role: "ADMIN", registrationTime: 2000, docs: 2 };
+        let user: User = { id: 1, name: "user1", email: "user1@email.com", role: "ADMIN", registrationTime: 2000, successDocs: 2, allDocs: 5 };
         expect(user.name).toEqual('user1');
     });
 
     it('user id test', () => {
-        let user: User = { id: 1, name: "user1", email: "user1@email.com", role: "ADMIN", registrationTime: 2000, docs: 2 };
+        let user: User = { id: 1, name: "user1", email: "user1@email.com", role: "ADMIN", registrationTime: 2000, successDocs: 2, allDocs: 5 };
         expect(user.id).toEqual(1);
     });
 
