5be2532f52c75fcf9674d9b75fb66a0a75f0b0bc
vlad <kalashnikov@ipoint.ru>
2016-05-11 08:26:18 +0800

    Change visible for field "LEVEL_OFFSET"

--- core/src/com/ipoint/coursegenerator/core/courseModel/blocks/textual/paragraph/HeaderBlock.java
+++ core/src/com/ipoint/coursegenerator/core/courseModel/blocks/textual/paragraph/HeaderBlock.java
@@ -24,8 +24,11 @@ public class HeaderBlock extends ParagraphBlock {
 	private static final int MIN_HEADER_LEVEL = 1;
 	// started from 1 because 0 is level of page header
 
-	private static final int LEVEL_OFFSET = 1;
-	// because in model header of page is 0, but in HTML it is 1
+	/**
+	 * Number of different between header of model and html. In model header of
+	 * page is 0, but in HTML it is 1
+	 */
+	public static final int LEVEL_OFFSET = 1;
 
 	public HeaderBlock(List<ParagraphItem> items) {
 		this(items, 1);
@@ -69,9 +72,11 @@ public class HeaderBlock extends ParagraphBlock {
 	 */
 	@Override
 	public Element toHtml(Document creatorTags) {
-		Element header = creatorTags.createElement("h".concat(String.valueOf(this.getLevel() + LEVEL_OFFSET)));
+		Element header = creatorTags.createElement("h".concat(String
+				.valueOf(this.getLevel() + LEVEL_OFFSET)));
 
-		for (AbstractContentItem<?> item : this.getItems().get(0).getValue().getItems()) {
+		for (AbstractContentItem<?> item : this.getItems().get(0).getValue()
+				.getItems()) {
 			if (item instanceof TextContentItem) {
 				Node text = item.toHtml(creatorTags).getFirstChild();
 				while (text.hasChildNodes()) {
