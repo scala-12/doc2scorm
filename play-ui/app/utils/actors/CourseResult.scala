b2fcaf9f6c32446713a99d9739c3df64bca032e9
vlad <kalashnikov@ipoint.ru>
2016-05-26 17:46:03 +0800

    Correction for case class

--- play-ui/app/utils/actors/CourseResult.scala
+++ play-ui/app/utils/actors/CourseResult.scala
@@ -6,8 +6,8 @@ import java.io.File
   * Created by kalas on 21.05.2016.
   */
 case class CourseResult(
-                         val bytes: Option[Array[Byte]],
-                         val fileName: Option[String],
-                         val successful: Boolean) {
+                         bytes: Option[Array[Byte]],
+                         fileName: Option[String],
+                         successful: Boolean) {
 
 }
