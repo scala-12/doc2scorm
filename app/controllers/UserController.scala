aeddaee6ae31fe00c0a9549f6da32bea23cf7889
vlad <kalashnikov@ipoint.ru>
2016-06-23 17:19:14 +0800

    Hotfixes: extra libs was removed, code fixes, DBUser as Option (not null)

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,30 +1,23 @@
 package controllers
 
-import play.api.i18n.MessagesApi
-import forms._
-import com.mohiva.play.silhouette.api.{ Environment, LogoutEvent, Silhouette }
+import java.util.Calendar
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.{Environment, Silhouette}
 import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
 import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
+import db.models.{DBUser, Tables}
+import db.models.daos.{ConversionDao, DBUserDao}
 import models.User
-import javax.inject.Inject
-import scala.concurrent.Future
-import play.api.mvc.Action
 import play.api.db.slick.DatabaseConfigProvider
-import slick.driver.JdbcProfile
-import db.models.Users
-import db.models.Tables
-import slick.driver.MySQLDriver.api._
-import scala.concurrent.ExecutionContext.Implicits.global
-import db.models.DBUser
-import java.util.Calendar
-import play.api.libs.json._
+import play.api.i18n.MessagesApi
 import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import slick.driver.JdbcProfile
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.language.postfixOps
-import db.models.DBConversion
-import db.models.daos.DBUserDao
-import db.models.daos.ConversionDao
-import scala.concurrent.duration._
-import scala.concurrent.Await
 
 class UserController @Inject() (
   val messagesApi: MessagesApi,
@@ -42,12 +35,12 @@ class UserController @Inject() (
   val dbConfig = dbConfigProvider.get[JdbcProfile]
 
   def dbUserToJson(dbUser: DBUser): Future[JsValue] = {
-    conversionDao successCount (dbUser.id) flatMap {
+    conversionDao.successCount(dbUser.id).flatMap {
       successCount =>
-        conversionDao allCount (dbUser.id) flatMap {
+        conversionDao.allCount(dbUser.id).flatMap {
           allCount =>
             Future successful {
-              dbUserToJson(dbUser, successCount, allCount);
+              dbUserToJson(dbUser, successCount, allCount)
             }
         }
     }
@@ -88,27 +81,27 @@ class UserController @Inject() (
 
     dbUserDao.getUserByEmail(request.identity.email.get).flatMap { user =>
       {
-        if (user != null) {
+        if (user.isDefined) {
           // get docs count
-          request.identity.role = Some(user.role)
-          request.identity.dbId = Some(user.id)
-          dbUserToJson(user).flatMap { json => Future.successful { Ok(json) } }
+          request.identity.role = Some(user.get.role)
+          request.identity.dbId = Some(user.get.id)
+          dbUserToJson(user.get).flatMap { json => Future.successful { Ok(json) } }
         } else {
           // insert new DBUser
-          var role: String = "GUEST";
-          var name: String = request.identity.fullName.get
-          var email: String = request.identity.email.get
+          var role: String = "GUEST"
+          val name: String = request.identity.fullName.get
+          val email: String = request.identity.email.get
 
           if (email.endsWith("@ipoint.ru")) {
             role = "ADMIN"
           }
           request.identity.role = Some(role)
           // Add new dbUser
-          var dbUser = new DBUser(0, name, email, role, Calendar.getInstance().getTimeInMillis())
+          val dbUser = DBUser(0, name, email, role, Calendar.getInstance().getTimeInMillis)
           dbUserDao.addUser(dbUser).flatMap { id =>
             dbUser.id = id
             request.identity.dbId = Some(id)
-            dbUserToJson(user).flatMap { json => Future.successful { Ok(json) } }
+            dbUserToJson(dbUser).flatMap { json => Future.successful { Ok(json) } }
           }
         }
 
@@ -136,8 +129,8 @@ class UserController @Inject() (
   def getUserById(id: Long) = SecuredAction.async { implicit request =>
     if ("ADMIN".equals(request.identity.role.get)) {
       dbUserDao.getUserById(id).flatMap { user =>
-        if (user != null) {
-          dbUserToJson(user).flatMap { json => Future.successful { Ok(json) } }
+        if (user.isDefined) {
+          dbUserToJson(user.get).flatMap { json => Future.successful { Ok(json) } }
         } else {
           Future.successful { Ok("") }
         }
@@ -150,9 +143,8 @@ class UserController @Inject() (
   def update(id: Long) = SecuredAction.async(parse.json) { implicit request =>
     if ("ADMIN".equals(request.identity.role.get)) {
       request.body.validate[(String, String)].map {
-        case (name, role) => {
+        case (name, role) =>
           dbUserDao.updateUserNameAndRole(id, name, role).flatMap { isUpdated => Future.successful(Ok("")) }
-        }
       }.recoverTotal {
         e => Future.successful { BadRequest("Detected error: " + e) }
       }
