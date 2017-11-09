7286c2a78ac966049b73ee2d7d74d50914e90928
vlad <kalashnikov@ipoint.ru>
2017-11-09 10:52:30 +0800

    updated method: use stream method 'anyMatch' instead 'filter.toArray.length != 0'

--- core/src/com/ipoint/coursegenerator/core/courseModel/content/blocks/exceptions/ChoiceBlockCreationException.java
+++ core/src/com/ipoint/coursegenerator/core/courseModel/content/blocks/exceptions/ChoiceBlockCreationException.java
@@ -8,8 +8,8 @@ import com.ipoint.coursegenerator.core.courseModel.content.blocks.questionsSecti
 public class ChoiceBlockCreationException extends BlockCreationException {
 
 	public ChoiceBlockCreationException(ChoiceBlock block, List<ChoiceItem> items) {
-		super(block, items, (items.stream().filter(item -> item.isCorrect()).toArray().length == 0)
-				? "Don't have correct answer" : null);
+		super(block, items,
+				(items.parallelStream().anyMatch(ChoiceItem::isCorrect)) ? null : "Don't have correct answer");
 	}
 
 	private static final long serialVersionUID = 7325216385954726851L;
